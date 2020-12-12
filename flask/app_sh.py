fa29dbdda0cdbab4e4cffd77a55d5060cff5e382
DESKTOP-MQU21B2\Yjieun <bbug9110@gmail.com>
2020-12-13 04:01:10 +0900

    ㅁㄴㅇㄹ

--- flask/app_sh.py
+++ flask/app_sh.py
@@ -114,6 +114,8 @@ def first():
     # 작업 마친 후 파일 저장
     wb.save("templates/주식데이터.xlsx")
     session.clear()
+    global code_count
+    code_count = 0
     return render_template("home.html")
 
 # 코스피
