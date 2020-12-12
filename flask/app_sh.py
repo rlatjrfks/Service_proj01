c4bfd827f0f2900e163d354a39ffe57f1e6f1895
DESKTOP-MQU21B2\Yjieun <bbug9110@gmail.com>
2020-12-12 15:00:36 +0900

    ip 주소 변환

--- flask/app_sh.py
+++ flask/app_sh.py
@@ -209,5 +209,4 @@ if __name__ == "__main__":
 
     app.debug = True
     app.config['DEBUG'] = True
-    app.run()
-    # app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000")
