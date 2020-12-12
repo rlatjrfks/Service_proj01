b4886a85d255b4cd9d0b6ad65ecb2ea579542364
DESKTOP-MQU21B2\Yjieun <bbug9110@gmail.com>
2020-12-12 16:37:31 +0900

    ㅁㄴㅇㄹ

--- flask/app_sh.py
+++ flask/app_sh.py
@@ -209,4 +209,5 @@ if __name__ == "__main__":
 
     app.debug = True
     app.config['DEBUG'] = True
-    app.run(host="0.0.0.0", port="5000")
+#    app.run(host="0.0.0.0", port="5000")
+    app.run()
