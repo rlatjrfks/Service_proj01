16a176baecb7f1580f261af5e874ddc23f98e954
DESKTOP-MQU21B2\Yjieun <bbug9110@gmail.com>
2020-12-14 20:02:11 +0900

    no message

--- flask/excel_to_db.py
+++ flask/excel_to_db.py
@@ -4,15 +4,21 @@ from openpyxl import load_workbook
 import numpy as np
 
 def insert_excel_to_db():
+    conn1 = pymysql.connect(
+        host='ls-360d5e5827a35e0a46fa340307d68f5a00a3b151.cvbhe0hq8rxv.ap-northeast-2.rds.amazonaws.com', port=3306,
+        user='dbmasteruser', passwd='Qa]HHh]dc1NsX>VLfo<=JA^1GcEWOCY$', db='dbmaster', charset='utf8')
+
+    sql1 = 'delete from jongmok_list'
+    cur1 = conn1.cursor()
+    cur1.execute(sql1)
+    conn1.commit()
+    
+
     conn = pymysql.connect(
         host='ls-360d5e5827a35e0a46fa340307d68f5a00a3b151.cvbhe0hq8rxv.ap-northeast-2.rds.amazonaws.com', port=3306,
         user='dbmasteruser', passwd='Qa]HHh]dc1NsX>VLfo<=JA^1GcEWOCY$', db='dbmaster', charset='utf8')
 
     try:
-        sql1 = 'delete from jongmok_list'
-        cur1 = conn.cursor()
-        cur1.execute(sql1)
-        conn.commit()
         with conn.cursor() as curs:
 
             sql = 'insert into jongmok_list values(%s, %s, %s)'
