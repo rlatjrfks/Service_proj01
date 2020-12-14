c43e71f95cc94ef2c2c9a3496c0e3e9960e17f09
DESKTOP-MQU21B2\Yjieun <bbug9110@gmail.com>
2020-12-14 19:26:16 +0900

    DB 수정

--- flask/excel_to_db.py
+++ flask/excel_to_db.py
@@ -9,20 +9,41 @@ def insert_excel_to_db():
         user='dbmasteruser', passwd='Qa]HHh]dc1NsX>VLfo<=JA^1GcEWOCY$', db='dbmaster', charset='utf8')
 
     try:
-        with conn.cursor() as curs:
-            sql = 'insert into jongmok_list values(%s, %s, %s)'
+        sql1 = 'select * from jongmok_list'
+        cur1 = conn.cursor()
+        cur1.execute(sql1)
+        if sql1:
+            with conn.cursor() as curs:
 
-            wb = load_workbook("templates/jusikdata.xlsx", data_only=True)
-            ws = wb['Sheet']
+                sql = "update jongmok_list SET jongmok_listcol='%s', jongmok_listcol1='%s' where jongmok_name='%s'"
 
-            iter_rows = iter(ws.rows)
-            next(iter_rows)
-            for row in iter_rows:
-                value = row[1].value.split(",")
-                vallist = ''.join(value)
-                value_float = row[2].value.split("%")
-                curs.execute(sql, (row[0].value, vallist, value_float[0]))
-            conn.commit()
+                wb = load_workbook("templates/jusikdata.xlsx", data_only=True)
+                ws = wb['Sheet']
+
+                iter_rows = iter(ws.rows)
+                next(iter_rows)
+                for row in iter_rows:
+                    value = row[1].value.split(",")
+                    vallist = ''.join(value)
+                    value_float = row[2].value.split("%")
+                    curs.execute(sql, (vallist, value_float[0], row[0].value))
+                conn.commit()
+        else:
+            with conn.cursor() as curs:
+
+                sql = 'insert into jongmok_list values(%s, %s, %s)'
+
+                wb = load_workbook("templates/jusikdata.xlsx", data_only=True)
+                ws = wb['Sheet']
+
+                iter_rows = iter(ws.rows)
+                next(iter_rows)
+                for row in iter_rows:
+                    value = row[1].value.split(",")
+                    vallist = ''.join(value)
+                    value_float = row[2].value.split("%")
+                    curs.execute(sql, (row[0].value, vallist, value_float[0]))
+                conn.commit()
     finally:
         conn.close()
 
